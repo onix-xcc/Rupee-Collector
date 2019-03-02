e13ddd4f51597e060ff426348d769bd706885a78
onix-xcc <onix.xcc@gmail.com>
2019-03-01 21:42:49 -0600

    Added reset.css and finished the assignment, almost cried towards the end over something minor.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,59 @@
-// Set rupee variables value range
-var greenRupee = fetchRupeeValue(1, 12);
-var blueRupee = fetchRupeeValue(1, 12);
-var redRupee = fetchRupeeValue(1, 12);
-var purpleRupee = fetchRupeeValue(1, 12);
+$(document).ready(function () {
 
-// Function for fetching the random value range
-function fetchRupeeValue(leastVal, greatestVal) {
-    return Math.floor(Math.random() * (leastVal - greatestVal) + leastVal);
-}
+    //Establish default variables for start of game and score tracking//
 
-//
+    var randoTargetNum = 0;
+    var wins = 0;
+    var losses = 0;
+    var trackRupeeTotal = 0;
+
+    //Rupee variables//
+
+    var greenRupee = 0;
+    var blueRupee = 0;
+    var redRupee = 0;
+    var purpleRupee = 0;
+
+    //Establish randomizing function for Rupees and target number//
+
+    function beginQuest() {
+        randoTargetNum = Math.floor(Math.random() * 120) + 10;
+        greenRupee = Math.floor(Math.random() * 12) + 1;
+        blueRupee = Math.floor(Math.random() * 12) + 1;
+        redRupee = Math.floor(Math.random() * 12) + 1;
+        purpleRupee = Math.floor(Math.random() * 12) + 1;
+
+        // JQuery assigns values and puts them on the HTML//
+        $("#genRandoNum").text(randoTargetNum);
+        $("#one").attr("assignRupeeVal", greenRupee);
+        $("#five").attr("assignRupeeVal", blueRupee);
+        $("#twenty").attr("assignRupeeVal", redRupee);
+        $("#fifty").attr("assignRupeeVal", purpleRupee);
+        trackRupeeTotal = 0;
+        $("#rupeesCollected").empty();
+    }
+
+    //Begin quest//
+    beginQuest();
+
+    $(".rupeeImg").on("click", function () {
+        var rupeeVal = ($(this).attr("assignRupeeVal"));
+        rupeeVal = parseInt(rupeeVal);
+        trackRupeeTotal += rupeeVal
+        $("#rupeesCollected").text(trackRupeeTotal);
+
+        if (trackRupeeTotal === randoTargetNum) {
+            alert("You have fulfilled your quest! Now go claim the Triforce!");
+            wins++;
+            $("#winCounter").text(wins);
+            beginQuest();
+        }
+
+        else if (trackRupeeTotal >= randoTargetNum) {
+            alert("Hyrule is doomed! Travel back in time and try again!");
+            losses++;
+            $("#lossCounter").text(losses);
+            beginQuest();}
+
+    })
+})
